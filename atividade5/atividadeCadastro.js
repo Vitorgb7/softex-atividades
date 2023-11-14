c44d9409984ef6b7c73e49c220b0da8e4c54536a
vitor <vitorgabriel181004@gmail.com>
2023-11-14 20:20:55 -0300

    atividade cadastro

---
+++ atividade5/atividadeCadastro.js
@@ -0,0 +1,11 @@
+// Declaração das variáveis
+var nome = "vitor gabriel"; 
+var idade = 19; 
+var salario = 3500.50; 
+var possuiDiploma = true; 
+
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Salário: R$ " + salario.toFixed(2)); 
+console.log("Possui diploma? " + possuiDiploma);
